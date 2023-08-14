61a8858f37a8bd51616e7f5673b769e02519b9e6
Andres Felipe Murcia Zapata <andresf.murciaz@gmail.com>
2023-08-14 08:38:36 -0500

    primer commit

---
+++ src/main/java/com/andruzsoft/appandruzsoft/AppAndruzsoftApplication.java
@@ -0,0 +1,13 @@
+package com.andruzsoft.appandruzsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAndruzsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAndruzsoftApplication.class, args);
+	}
+
+}
